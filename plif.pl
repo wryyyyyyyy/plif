87cbaaa05ad43dfd60d9ad6ce83aaba6eb5f0acd
wryyyyyyyy <33553950+wryyyyyyyy@users.noreply.github.com>
2020-12-16 14:46:00 +0100

    Added README.md
    first commit

---
+++ plif.pl
@@ -0,0 +1,10 @@
+#!/bin/perl
+
+my @iface = (`ip address show` =~ /(.*?)\:\s*(.*?)\:\s*\<BROADCAST(.*?)/g);
+print "INTERFACE:\t".$2."\n"; # valid iface
+
+my @ip = (`ip route show` =~ /(\S+)/g);
+print "IP ADDRESS:\t".$1."\n";
+
+my @mac = (`ip link show` =~ /link\/ether\s*(\S+)/g);
+print "MAC ADDRESS:\t".$1."\n";
