1e55a939eadd05559afb701eb238ae44bf45e279
wryyyyyyyy <33553950+wryyyyyyyy@users.noreply.github.com>
2020-12-18 04:39:16 +0100

    HOPE IS BETTER FORMATTED YET

--- protobot.py
+++ protobot.py
@@ -19,7 +19,9 @@ import ssl
 
 
 """
-# will be threaded or os.fork(ed) a bit later
+	# потоки или потомки?
+       вот в чем главный вопрос
+=========================================
 #def parent_child():
   #n = os.fork()
 
@@ -34,8 +36,6 @@ import ssl
 # Driver code
 #parent_child()
 """
-
-
 
 server = 'chat.freenode.net'
 port = 6697
@@ -58,48 +58,54 @@ def joinchan(chan):
   ircsock.send('JOIN ' + chan + 'n')
   ircsock.send('PRIVMSG ' + channel + ' :Консолька, ищи ботов' + 'n') #> onjoin message
 
-#> def topic(chan):
-#> tpc = [ircmsg] for items in tpc:
-#> print(items)
-#> ircsock.send("PRIVMSG "+ channel + " :"+ "" +"\n")
+"""
+ def topic(chan):
+ tpc = [ircmsg] for items in tpc:
+ print(items)
+ ircsock.send("PRIVMSG "+ channel + " :"+ "" +"\n")
+"""
 
 def wat():
   ircsock.send('PRIVMSG ' + channel + ' :Да он же у вас однопоточный!' + 'n') #> иногда даже работает
 
 def f_():
-  fh = open('u.txt') # морская улиточка в ASCII
+  fh = open('u.txt')
   for line in fh:
     #print(line.rstrip()) # можно смотреть в сосноль
     ircsock.send('NOTICE ' + channel + ' :' + line + 'n')
   fh.close()
-  
-  ircsock.send('rn') #> иногда не срабатывает функция выше
-		     #> улиточка. может дело в регэкспе,
-		     #> или сокету явно чего-то не хватает
 
-#>============================================================================= >
-#>try to connect
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((server, port))
-ircsock = ssl.wrap_socket(s)
-url = ('USER ' + username + ' ' + hostname +' '+ srvrname +' :'+ realname + 'n')
-ircsock.send(bytes(url.encode()))
-url = ('NICK ' + botnick + 'n')
-ircsock.send(bytes(url.encode()))
+# иногда не срабатывает функция выше. может дело в регэкспе,
+# или сокету явно чего-то не хватает. поэтому пробуем это решение
 
-while 1:
-  ircmsg = ircsock.recv(2048)
-  ircmsg = ircmsg.strip('nr')
-  print(ircmsg)
+ircsock.send('rn')
 
-  if(ircmsg.find('376')) != -1: # :END of /MOTD + JOIN CHANNEL
-  #joinchan(channel)
+#>==================================================# > try to connect < #================================================= >
+## init socket and try to connect ##		      # try to connect < #
+						      # Nice Lok
 
-  if(ircmsg.find(':xzibit:')) != -1:
-    wat()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # wow! we got a socket handle
+s.connect((server, port)) 			      # try to connect our network
+ircsock = ssl.wrap_socket(s)			      # it should be more secure
+url = ('USER ' + username + ' ' + hostname + ' ' + srvrname + ' :' + realname + 'n' )
+ircsock.send(bytes(url.encode()))		      # эта строка успешно отослана
+url = ('NICK ' + botnick + 'n')			      # подготовим другую,закончим ее 
+ircsock.send(bytes(url.encode()))		      # затем отошлем по тому же адресу
 
+while 1:					      # войдем в цикл
+ircmsg = ircsock.recv(2048)			      # выделим размер буфера
+ircmsg = ircmsg.strip('nr')			      # вырежем симводы переноса строки
+print(ircmsg)					      # и выведем пользователю
 
-  if(ircmsg.find(':Майор, улиточку')) != -1: # 
+if(ircmsg.find('376')) != -1: 		      	      # END of /MOT
+						      # JOIN CHANNEL
+						      # 
+if(ircmsg.find(':xzibit:')) != -1:                    #
+    wat()					      #
+						      #
+						      #
+  if(ircmsg.find(':Майор, улиточку')) != -1:          #
+		
     f_()
 
 
