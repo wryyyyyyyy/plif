65ea370f5999ac2e6e5e291b548686a9812ac95f
wryyyyyyyy <no@spam.here>
2020-12-21 14:55:18 +0100

    major defined

--- protobot.py
+++ protobot.py
@@ -9,9 +9,9 @@
 
 
 """
-
 import sys
 import socket
+import time
 import ssl
 
 ## vars ##
@@ -28,6 +28,29 @@ ssock = ssl.wrap_socket(s)
 ssock.send(bytes("NICK %s\r\n" % botnick, "UTF-8"))
 ssock.send(bytes("USER %s %s blah :%s\r\n" % (botnick, server, botnick), "UTF-8"))
 
+ul = '''
+я — морская   @             _________
+улиточка       \____       /         \
+        \      /    \     /   ____    \
+               \_    \   /   /    \    \
+                 \    \ (    \__/  )    )
+                  \    \_\ \______/    /
+                   \      \           /___
+                    \______\_________/____-_
+'''
+
+def major():
+  #print(ul)
+  #ssock.send(bytes("PRIVMSG %s :%s\n" % (channel, ul), "UTF-8"))
+  fh = open('u.txt')
+  for line in fh:
+    ssock.send(bytes("PRIVMSG %s :%s\n" % (channel, line), "UTF-8"))
+  fh.close()
+  cooldown() # some antiflood routine shall be implemented here
+
+def cooldown():
+  time.sleep(5)
+
 while 1:
   buf = buf+ssock.recv(1024).decode("UTF-8")
   tmp = str.split(buf, "\n")
@@ -43,3 +66,7 @@ while 1:
 
     if(line[0] == "PING"):
       ssock.send(bytes("PONG %s\r\n" % line[1], "UTF-8"))
+
+  out = str(tmp)
+  if(out.find("Майор, улиточку")) != -1:
+    major()
